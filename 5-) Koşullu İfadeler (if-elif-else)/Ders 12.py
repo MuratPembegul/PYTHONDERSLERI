fcf55cb4dcfab4a611c515e55e62250d0a029358
Murat Pembegül <mpembegul15@gmail.com>
2023-08-20 20:24:05 +0300

    Add files via upload

---
+++ 5-) Koşullu İfadeler (if-elif-else)/Ders 12.py
@@ -0,0 +1,12 @@
+# Koşullu ifadeleri kontrol etmek için
+# kullanılır. (if-elif-else)
+# Örnek: Mini Yaş Programı
+yas = int(input("Yaşınızı girin: "))
+
+if yas < 18:
+    print("Yaşınız 18 den küçük")
+elif yas == 18:
+    print("Yaşınız 18")
+else:
+    print("Yaşınız 18 den büyük")
+    

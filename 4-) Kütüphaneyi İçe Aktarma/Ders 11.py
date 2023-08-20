1a9ce9822089a67f823256660bf80d0c148e55b3
Murat Pembegül <mpembegul15@gmail.com>
2023-08-20 20:21:47 +0300

    Add files via upload

---
+++ 4-) Kütüphaneyi İçe Aktarma/Ders 11.py
@@ -0,0 +1,15 @@
+# Bir anahtar import modülünü içe aktarma.
+# Bir modülü içe aktarmak için kullanılır.
+
+# Math kütüphanesini içe aktarma
+import math
+# Math kütüphanesinin tamamını içe aktarma
+from math import*
+# Math kütüphanesinin kısaltılmış adı ile içe aktarma
+import math as m
+# Math kütüphanesini kullanarak formül bulma
+import math 
+sayı = 25
+karakök = math.sqrt (sayı)
+
+print(f"{sayı} sayısının karakökü: {karakök}")

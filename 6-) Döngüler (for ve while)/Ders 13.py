e6ccb1f50ee6a4807c495522d8192100ea2a7927
Murat Pembegül <mpembegul15@gmail.com>
2023-08-20 20:27:32 +0300

    Add files via upload

---
+++ 6-) Döngüler (for ve while)/Ders 13.py
@@ -0,0 +1,17 @@
+"""Döngüler bizim belirli bir kod
+bloğunun tekrar tekrar çalışmasını
+sağlayan yapılardır."""
+# For döngüsü belirli bir elemanları gezinmek için
+# kullanılır.
+# Örnek: Listeler de for döngüsü
+şehirler = ["İstanbul", "Ankara", 
+"İzmir", "Bursa", "Adana"]
+for şehir in şehirler:
+    print(şehir)
+#  Bir koşulu sağladığı sürece döngüyü çalıştırmak
+#  için kullanılır.
+# Örnek: Sayılar da while döngüsü 
+sayı = 1
+while sayı <= 5:
+    print(sayı)
+    sayı += 1

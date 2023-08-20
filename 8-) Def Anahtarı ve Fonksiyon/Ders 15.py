6c9d68f7b22a94c67a0de4a17c721818ee3da519
Murat Pembegül <mpembegul15@gmail.com>
2023-08-20 20:33:46 +0300

    Add files via upload

---
+++ 8-) Def Anahtarı ve Fonksiyon/Ders 15.py
@@ -0,0 +1,13 @@
+#Def anahtarı,bir fonksiyon tanımlamak için kullanılır.
+""" Aşağıda ki kod parçacığı, "selamla" adında bir 
+fonksiyon tanımlar. Bu fonksiyon, "isim" adında bir
+parametre alır ve verilen isme göre bir selam 
+mesajı yazdırır."""
+# Örnek:
+def selamla(isim):
+    """Verilen isme göre selam mesajı gönderir."""
+    print(f"Merhaba, {isim}!")
+
+# Fonksiyonu çağırma
+selamla("Ahmet")
+selamla("Mehmet")

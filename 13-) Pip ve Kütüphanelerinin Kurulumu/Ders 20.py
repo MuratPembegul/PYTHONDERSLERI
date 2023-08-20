adf2bb495d6d58abacfc16064d9df7639fcac3a7
Murat Pembegül <mpembegul15@gmail.com>
2023-08-20 20:37:44 +0300

    Add files via upload

---
+++ 13-) Pip ve Kütüphanelerinin Kurulumu/Ders 20.py
@@ -0,0 +1,10 @@
+# Pip yüklü değilse pip yüklüyoruz
+# get-pip.py
+# Pip versionunu öğreniyoruz
+# pip --version
+# Pip güncellemek için bunu kullanıyoruz
+# pip install --upgrade pip
+# Pandas, Numpy, Matplotlib kütüphanelerini kuruyoruz
+# pip install pandas
+# pip install numpy
+# pip install matplotlib
